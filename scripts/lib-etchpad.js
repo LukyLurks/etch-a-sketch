209669980dbfca0702a1b67dd0538ea3727541a8
Luckner Cyné <cyne.luckner@gmail.com>
2019-05-28 14:53:41 +0200

    Grid cells squared (not rectangle)

--- scripts/lib-etchpad.js
+++ scripts/lib-etchpad.js
@@ -1,9 +1,9 @@
 define(function() {
 
-  const createGrid = function(grid, size) {
+  const createGrid = function(grid, gridSize) {
     let cell = undefined;
-    for(let i = 0; i < size; i++) {
-      for(let j = 0; j < size; j++) {
+    for(let i = 0; i < gridSize; i++) {
+      for(let j = 0; j < gridSize; j++) {
         cell = document.createElement('div');
         cell.classList.add('cell');
         grid.appendChild(cell);
@@ -12,8 +12,30 @@ define(function() {
     }
     return grid;
   }
+  
+  const getCellSize = function(grid, gridSize) {
+    let availableWidth = grid.parentNode.clientWidth;
+    let availableHeight = grid.parentNode.clientHeight;
+    let shortest = Math.min(availableWidth, availableHeight);
+    return Math.floor(shortest / gridSize);
+  }
+
+  const resizeCells = function(grid, gridSize) {
+    newSize = getCellSize(grid, gridSize);
+    Array.from(grid.children).forEach((child) => {
+      if(child.classList.contains('cell')) {
+        child.style.width = newSize + 'px';
+        child.style.height = newSize + 'px';
+      }
+    });
+    // removes space between rows
+    grid.style.lineHeight = 0;
+    return newSize;
+  }
 
   return {
-    createGrid
+    createGrid,
+    getCellSize,
+    resizeCells
   }
 });
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 requirejs(['lib-etchpad'], function(libEtchpad) {
   const defaultSize = 16;
   const grid = document.querySelector('#grid');
+  let gridSize = defaultSize;
 
-  libEtchpad.createGrid(grid, defaultSize);
+  libEtchpad.createGrid(grid, gridSize);
+  libEtchpad.resizeCells(grid, gridSize);
 });
